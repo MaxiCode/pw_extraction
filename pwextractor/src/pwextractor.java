e26b8d3c129988d6290b3342dcb1435087685213
ChrisHx <chris.hornischer@uni-weimar.de>
2014-06-13 13:16:37 +0200

    src Update

--- pwextractor/src/pwextractor.java
+++ pwextractor/src/pwextractor.java
@@ -1,10 +1,18 @@
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.zip.GZIPInputStream;
-import edu.cmu.lemurproject.WarcRecord;
-import edu.cmu.lemurproject.WarcHTMLResponseRecord;
+
+//import de.l3s.boilerpipe.extractors.ArticleExtractor;
+//import edu.cmu.lemurproject.WarcRecord;
+//import edu.cmu.lemurproject.WarcHTMLResponseRecord;
+
+import net.htmlparser.jericho.*;
+
 
 public class pwextractor {
 
@@ -16,11 +24,12 @@ public class pwextractor {
     // cast to a data input stream
     DataInputStream inStream=new DataInputStream(gzInputStream);
     
-    // iterate through our stream
-    WarcRecord thisWarcRecord;
+    // iterate through our stream  
+    
+    /*WarcRecord thisWarcRecord;
     while ((thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
       // see if it's a response record
-      if (thisWarcRecord.getHeaderRecordType().equals("response")) {
+      /*if (thisWarcRecord.getHeaderRecordType().equals("response")) {
         // it is - create a WarcHTML record
         WarcHTMLResponseRecord htmlRecord=new WarcHTMLResponseRecord(thisWarcRecord);
         // get our TREC ID and target URI
@@ -28,9 +37,26 @@ public class pwextractor {
         String thisTargetURI=htmlRecord.getTargetURI();
         // print our data
         System.out.println(thisTRECID + " : " + thisTargetURI);
+    	
       }
-    }
+    }*/
     
+    
+    File file = new File("output.txt");  
+    FileOutputStream fis = new FileOutputStream(file);  
+    PrintStream out = new PrintStream(fis);
+    
+    Source source=new Source(inStream);
+	System.setOut(out);  
+	//System.out.println(source.getTextExtractor().setIncludeAttributes(true).toString());
+    
+	/*TextExtractor textExtractor=new TextExtractor(source) {
+		public boolean excludeElement(StartTag startTag) {
+			return startTag.getName()==HTMLElementName.P || "control".equalsIgnoreCase(startTag.getAttributeValue("class"));
+		}
+	};
+	System.out.println(textExtractor.setIncludeAttributes(true).toString());*/
+	
     inStream.close();
   }
 }
