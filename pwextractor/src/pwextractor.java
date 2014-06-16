0385eaaacaa08ed1ac2e76b02dd7f6ea0a157d7b
ChrisHx <chris.hornischer@uni-weimar.de>
2014-06-16 16:43:06 +0200

    src Update

--- pwextractor/src/pwextractor.java
+++ pwextractor/src/pwextractor.java
@@ -3,60 +3,72 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+//import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.zip.GZIPInputStream;
+import java.net.URL;
 
-//import de.l3s.boilerpipe.extractors.ArticleExtractor;
+
+//import org.xml.sax.InputSource;
+//import java.util.zip.GZIPInputStream;
+
+import de.l3s.boilerpipe.BoilerpipeProcessingException;
 //import edu.cmu.lemurproject.WarcRecord;
 //import edu.cmu.lemurproject.WarcHTMLResponseRecord;
+import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
-import net.htmlparser.jericho.*;
+//import net.htmlparser.jericho.*;
 
 
 public class pwextractor {
 
-  public static void main(String[] args) throws IOException {
-    String inputWarcFile=args[0];
-    // open our gzip input stream
-    GZIPInputStream gzInputStream=new GZIPInputStream(new FileInputStream(inputWarcFile));
+  public static void main(String[] args) throws IOException, BoilerpipeProcessingException {
+    
+	File file = new File("output.txt");  //Output file name
+	FileOutputStream fis = new FileOutputStream(file);
+	PrintStream out = new PrintStream(fis);
+	
+	//String inputWarcFile=args[0];
+	    
+    // open gzip input stream
+    //GZIPInputStream gzInputStream=new GZIPInputStream(new FileInputStream(inputWarcFile));
     
     // cast to a data input stream
-    DataInputStream inStream=new DataInputStream(gzInputStream);
-    
+    //DataInputStream inStream=new DataInputStream(gzInputStream);
+    //String inStream2 = inStream.toString();
+    /*
     // iterate through our stream  
-    
-    /*WarcRecord thisWarcRecord;
+    WarcRecord thisWarcRecord;
     while ((thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
       // see if it's a response record
-      /*if (thisWarcRecord.getHeaderRecordType().equals("response")) {
+      if (thisWarcRecord.getHeaderRecordType().equals("response")) {
         // it is - create a WarcHTML record
         WarcHTMLResponseRecord htmlRecord=new WarcHTMLResponseRecord(thisWarcRecord);
-        // get our TREC ID and target URI
-        String thisTRECID=htmlRecord.getTargetTrecID();
-        String thisTargetURI=htmlRecord.getTargetURI();
-        // print our data
-        System.out.println(thisTRECID + " : " + thisTargetURI);
-    	
+        
+        
+        
       }
-    }*/
+    }
+    */
     
+
     
-    File file = new File("output.txt");  
-    FileOutputStream fis = new FileOutputStream(file);  
-    PrintStream out = new PrintStream(fis);
-    
-    Source source=new Source(inStream);
-	System.setOut(out);  
-	//System.out.println(source.getTextExtractor().setIncludeAttributes(true).toString());
-    
-	/*TextExtractor textExtractor=new TextExtractor(source) {
-		public boolean excludeElement(StartTag startTag) {
-			return startTag.getName()==HTMLElementName.P || "control".equalsIgnoreCase(startTag.getAttributeValue("class"));
-		}
-	};
-	System.out.println(textExtractor.setIncludeAttributes(true).toString());*/
+	//FileReader fr = new FileReader("/home/chris/workspace/WARC/snowden.warc.gz");
+	URL url = new URL("http://www.spiegel.de/politik/ausland/edward-snowden-im-interview-mit-nbc-whistleblower-als-patriot-a-972350.html");
+    String text = ArticleExtractor.INSTANCE.getText(url);
 	
-    inStream.close();
+    System.setOut(out);  		     			  //write output to file
+    
+	String[] lines = text.split("[.]+");  		  //Split string into lines 
+	for(int i=0; i<lines.length; i++){		    
+		StringBuffer result = new StringBuffer();
+		result.append(lines[i]);
+		String pwStr = result.toString();		  //Convert Stringbuffer Array into String
+		System.out.println(pwStr);				  //Print Array
+	}
+    
+	
+	//inStream.close();
+	
   }
 }
